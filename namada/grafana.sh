9ddea91d9b7957b11b51d1ff087b0a659d3a4361
encipher <encipher01@yandex.ru>
2023-05-22 16:43:24 +0300

    Update and rename grafana.json to grafana.sh

---
+++ namada/grafana.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+green="\e[32m"
+pink="\e[35m"
+reset="\e[0m"
+
+touch .bash_profile
+source .bash_profile
+echo -e "${green}************************${reset}"
+# Update and upgrade the system
+echo -e "${green}*************Update and upgrade the system*************${reset}"
+
+apt-get update -y
+apt-get upgrade -y
+
+# Install necessary dependencies
+echo -e "${green}*************Install necessary dependencies*************${reset}"
+apt-get install -y curl tar wget original-awk gawk
+
+set -e
+
+# Ensure the script is run as root
+echo -e "${green}*************Ensure the script is run as root*************${reset}"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+# Function to check the status of a service
+echo -e "${green}*************Function to check the status of a service***********${reset}"
+check_service_status() {
+  service_name="$1"
+  if systemctl is-active --quiet "$service_name"; then
+    echo "$service_name is running."
+  else
+    echo "$service_name is not running."
+  fi
+}
+
+
+# Create necessary directories
+echo -e "${green}*************Create necessary directories***********${reset}"
+
+# An array of directories to be created
+directories=("/var/lib/prometheus" "/etc/prometheus/rules" "/etc/prometheus/rules.d" "/etc/prometheus/files_sd")
+
+# Iterate through the array
+for dir in "${directories[@]}"; do
+  # Check if directory exists and is not empty
+  if [ -d "$dir" ] && [ "$(ls -A $dir)" ]; then
+    echo "$dir already exists and is not empty. Skipping..."
+  else
+    mkdir -p "$dir"
+    echo "Created directory: $dir"
+  fi
+done
+
+
+# Download and extract Prometheus
+echo -e "${green}*************Download and extract Prometheus***********${reset}"
+cd /tmp
+curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+tar xvf prometheus*.tar.gz
+cd prometheus*/
+
+if [ -d "/etc/prometheus/consoles" ] && [ "$(ls -A /etc/prometheus/consoles)" ]; then
+  echo "/etc/prometheus/consoles directory exists and is not empty. Skipping..."
+else
+  mv consoles /etc/prometheus/
+fi
+
+if [ -d "/etc/prometheus/console_libraries" ] && [ "$(ls -A /etc/prometheus/console_libraries)" ]; then
+  echo "/etc/prometheus/console_libraries directory exists and is not empty. Skipping..."
+else
+  mv console_libraries /etc/prometheus/
+fi
+
+mv prometheus promtool /usr/local/bin/
+
+
+# Define the content to be added
+echo -e "${green}**************Define the content to be added**********${reset}"
+if [ -f "/etc/prometheus/prometheus.yml" ]; then
+  rm "/etc/prometheus/prometheus.yml"
+fi
+sudo tee /etc/prometheus/prometheus.yml<<EOF
+global:
+  scrape_interval: 15s
+  evaluation_interval: 15s
+alerting:
+  alertmanagers:
+    - static_configs:
+        - targets: []
+
+rule_files: []
+scrape_configs:
+  - job_name: "prometheus"
+    metrics_path: /metrics
+    static_configs:
+      - targets: ["localhost:9100"]
+  - job_name: "namada"
+    scrape_interval: 5s
+    metrics_path: /
+    static_configs:
+      - targets: ['localhost:26661']
+EOF
+
+# Create Prometheus service
+echo -e "${green}************Create Prometheus service***********${reset}"
+
+sudo tee /etc/systemd/system/prometheus.service<<EOF
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+[Service]
+Type=simple
+User=root
+ExecReload=/bin/kill -HUP \$MAINPID
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/var/lib/prometheus \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# Reload systemd, enable, and start Prometheus
+echo -e "${green}**************Reload systemd, enable, and start Prometheus**********${reset}"
+systemctl daemon-reload
+systemctl enable prometheus
+systemctl start prometheus
+
+# Check Prometheus service status
+echo -e "${green}***********Check Prometheus service status*************${reset}"
+check_service_status "prometheus"
+
+# Install Grafana
+echo -e "${green}**************Install Grafana**********${reset}"
+apt-get install -y apt-transport-https software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+echo "deb https://packages.grafana.com/enterprise/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update -y
+apt-get install grafana-enterprise -y
+systemctl daemon-reload
+systemctl enable grafana-server
+systemctl start grafana-server
+
+# Check Grafana service status
+echo -e "${green}************Check Grafana service status************${reset}"
+check_service_status "grafana-server"
+
+# Get the real IP address of the server
+echo -e "${green}*************Get the real IP address of the server***********${reset}"
+real_ip=$(hostname -I | awk '{print $1}')
+
+# Check Prometheus service status again
+echo -e "${green}************Check Prometheus service status again************${reset}"
+check_service_status "prometheus"
+
+# Install and start Prometheus Node Exporter
+echo -e "${green}*************Install and start Prometheus Node Exporter***********${reset}"
+apt install prometheus-node-exporter -y
+systemctl start prometheus-node-exporter
+systemctl enable prometheus-node-exporter
+
+# Change config
+echo -e "${green}*************Change config prometheus ON ***********${reset}"
+file_path="$HOME/.local/share/namada/public-testnet-8.0.b92ef72b820/config.toml"
+search_text="instrumentation_prometheus = false"
+replacement_text="instrumentation_prometheus = true"
+
+# Check if the replacement text already exists in the file
+if grep -qFx "$replacement_text" "$file_path"; then
+  echo "Replacement text already exists. No changes needed."
+else
+  # Replace the search text with the replacement text
+  sed -i "s/$search_text/$replacement_text/g" "$file_path"
+  echo "Text replaced successfully."
+fi
+
+
+# Reload systemd
+echo -e "${green}**************RESTART systemd**********${reset}"
+systemctl restart prometheus
+systemctl restart grafana-server
+systemctl restart namadad
+
+echo -e "${green}*************Install OK***********${reset}"
+echo -e "${green}**********************************${reset}"
+echo -e "${green}**********************************${reset}"
+echo -e "${green}Grafana is accessible at: http://$real_ip:3000${reset}"
+echo -e "${pink}Login credentials:${reset}"
+echo -e "${pink}---------Username:${reset} admin"
+echo -e "${pink}---------Password:${reset} admin"
+echo -e "${green}**********************************${reset}"
+echo -e "${pink} Open grafana and add to Home/Connections/Your_connections/Data_sources  ${reset}"
+echo -e "${pink} ...new source prometheus with address http://localhost:9090  ${reset}"
+echo -e "${pink} ...click SAVE and TEST button  ${reset}"
+echo -e "${pink} ...then import to Home/Dashboards/Import_dashboard new dashboard    ${reset}"
+echo -e "${pink} ...Import via grafana.com   ID = 18401    ${reset}"
+echo -e "${green}**********************************${reset}"
