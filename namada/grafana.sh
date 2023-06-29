5d77db13f67cba41ffc8ad797d54b841858f6d9b
encipher <encipher01@yandex.ru>
2023-06-29 16:44:25 +0300

    Update grafana.sh

--- namada/grafana.sh
+++ namada/grafana.sh
@@ -64,10 +64,10 @@ done
 # Download and extract Prometheus
 echo -e "${green}*************Download and extract Prometheus***********${reset}"
 cd $HOME
-rm prometheus*.tar.gz
 wget https://github.com/prometheus/prometheus/releases/download/v2.45.0/prometheus-2.45.0.linux-amd64.tar.gz
 sleep 1
 tar xvf prometheus*.tar.gz
+rm prometheus*.tar.gz
 cd prometheus*/
 
 if [ -d "/etc/prometheus/consoles" ] && [ "$(ls -A /etc/prometheus/consoles)" ]; then
